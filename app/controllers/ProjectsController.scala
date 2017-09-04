2339a651e3463353a3d54be63ce439f7e04ff9ec
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-09-04 11:43:57 +0200

    implemented project creation endpoint

---
+++ app/controllers/ProjectsController.scala
@@ -0,0 +1,80 @@
+package controllers
+
+import javax.inject.{ Inject, Singleton }
+
+import authorization.{ JWTVerifierProvider, ResourcesManagerJWTVerifierProvider }
+import ch.datascience.graph.elements.mutation.create.CreateVertexOperation
+import ch.datascience.graph.elements.mutation.log.model.json._
+import ch.datascience.graph.elements.mutation.log.model.{ EventStatus, MutationFailed, MutationResponse, MutationSuccess }
+import ch.datascience.graph.elements.mutation.{ GraphMutationClient, Mutation }
+import ch.datascience.graph.elements.new_.build.NewVertexBuilder
+import ch.datascience.graph.naming.NamespaceAndName
+import ch.datascience.graph.values.StringValue
+import ch.datascience.service.ResourceManagerClient
+import ch.datascience.service.models.projects.CreateProjectRequest
+import ch.datascience.service.models.projects.json._
+import ch.datascience.service.models.resource.json.AccessRequestFormat
+import ch.datascience.service.security.ProfileFilterAction
+import ch.datascience.service.utils.persistence.graph.{ GraphExecutionContextProvider, JanusGraphTraversalSourceProvider }
+import ch.datascience.service.utils.persistence.reader.VertexReader
+import ch.datascience.service.utils.{ ControllerWithBodyParseJson, ControllerWithGraphTraversal }
+import play.api.libs.json.{ JsObject, Json }
+import play.api.libs.ws.WSClient
+import play.api.mvc._
+
+import scala.concurrent.{ ExecutionContext, Future }
+
+@Singleton
+class ProjectsController @Inject() (
+    config:                                         play.api.Configuration,
+    jwtVerifier:                                    JWTVerifierProvider,
+    rmJwtVerifier:                                  ResourcesManagerJWTVerifierProvider,
+    graphMutationClientProvider:                    GraphMutationClientProvider,
+    implicit val wsclient:                          WSClient,
+    implicit val graphExecutionContextProvider:     GraphExecutionContextProvider,
+    implicit val janusGraphTraversalSourceProvider: JanusGraphTraversalSourceProvider,
+    implicit val vertexReader:                      VertexReader,
+    implicit val ec:                                ExecutionContext
+) extends Controller with ControllerWithBodyParseJson with ControllerWithGraphTraversal {
+
+  lazy val gc: GraphMutationClient = graphMutationClientProvider.get
+
+  def projectCreate: Action[CreateProjectRequest] = ProfileFilterAction( jwtVerifier.get ).async( bodyParseJson[CreateProjectRequest] ) { implicit request =>
+    val token: String = request.headers.get( "Authorization" ).getOrElse( "" )
+    val rmc = new ResourceManagerClient( config )
+    val name = request.body.name
+    val extra = Some( Json.toJson( Map(
+      "project" -> name
+    ) ).as[JsObject] )
+
+    rmc.authorize( AccessRequestFormat, request.body.toAccessRequest( extra ), token ).flatMap { res =>
+      val optResult: Option[Future[Result]] = res.map { ag =>
+        if ( ag.verifyAccessToken( rmJwtVerifier.get ).extraClaims.equals( extra ) ) {
+          val vertex = new NewVertexBuilder()
+            .addSingleProperty( "project:project_name", StringValue( name ) )
+            .addSingleProperty( "resource:owner", StringValue( request.userId ) )
+            .addType( NamespaceAndName( "project:project" ) )
+          val mut = Mutation( Seq( CreateVertexOperation( vertex.result() ) ) )
+          gc.postAndWait( mut ).map { ev =>
+            val response = ev.status match {
+              case EventStatus.Completed( res ) => res
+              case EventStatus.Pending          => throw new RuntimeException( s"Expected completed mutation: ${ev.uuid}" )
+            }
+
+            val mutationResponse = response.event.as[MutationResponse]
+            val projectVertexId = mutationResponse match {
+              case MutationSuccess( results ) => results.head
+              case MutationFailed( reason )   => throw new RuntimeException( s"Project creation failed, caused by: $reason" )
+            }
+
+            Created( projectVertexId )
+          }
+        }
+        else Future( InternalServerError( "Resource Manager response is invalid." ) )
+      }
+
+      optResult.getOrElse( Future( InternalServerError( "No response from Resource Manager." ) ) )
+    }
+  }
+
+}
